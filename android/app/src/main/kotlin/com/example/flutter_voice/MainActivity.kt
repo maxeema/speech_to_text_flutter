MarcusNg/flutter_voice

Flutter Speech to Text App Tutorial

android/app/src/main/kotlin/com/example/flutter_voice/MainActivity.kt
@@ -1,0 +1,6 @@
+package com.example.flutter_voice
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}

maxeema/speech_to_text_flutter

android/app/src/main/kotlin/com/example/flutter_voice/MainActivity.kt
@@ -1,0 +1,6 @@
+package com.example.speech_to_text_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}